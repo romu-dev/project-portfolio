797bb1955664e32735fdbd235e4eadf3bf1bcb78
romu-dev <r.hiraux@codeur.online>
2022-05-11 16:46:36 +0200

    insert into db

--- handler.php
+++ handler.php
@@ -5,5 +5,11 @@ session_start();
 $title = strip_tags( $_POST["data-title"]);
 $description = strip_tags( $_POST["data-description"]);
 
-echo $title;
-echo $description;
+require_once("db_connect.php");
+$sql = "INSERT INTO tbl_projects (project_title, project_description) VALUES (:project_title, :project_description)";
+
+$query = $db->prepare($sql);
+$query->bindValue(':project_title', $title, PDO::PARAM_STR);
+$query->bindValue(':project_description', $description, PDO::PARAM_STR);
+
+$query->execute();
